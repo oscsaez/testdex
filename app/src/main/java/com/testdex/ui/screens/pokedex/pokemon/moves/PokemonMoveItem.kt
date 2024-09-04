87e4e87a4bbc44af444f22f8a4ed64fc47b2dede
oscsaez <oscar.sg.dnh@gmail.com>
2024-09-04 13:42:47 +0200

    🎨 Add moves list on pokemon screen

--- app/src/main/java/com/testdex/ui/screens/pokedex/pokemon/moves/PokemonMoveItem.kt
+++ app/src/main/java/com/testdex/ui/screens/pokedex/pokemon/moves/PokemonMoveItem.kt
@@ -3,13 +3,16 @@ package com.testdex.ui.screens.pokedex.pokemon.moves
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.testdex.R
 import com.testdex.ui.model.MoveUIModel
 import com.testdex.ui.theme.Gray
@@ -24,7 +27,9 @@ fun PokemonMoveItem(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            modifier = Modifier.weight(2f),
+            modifier = Modifier
+                .weight(2.6f)
+                .padding(start = dimensionResource(id = R.dimen.screen_padding)),
             text = move.name,
             style = MaterialTheme.typography.bodyLarge,
             color = MaterialTheme.colorScheme.onBackground
@@ -34,26 +39,35 @@ fun PokemonMoveItem(
             contentDescription = "${move.type.name} type icon"
         )
         Text(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = dimensionResource(id = R.dimen.regular_padding)),
             text = move.pp.toString(),
             style = MaterialTheme.typography.bodyMedium,
-            color = Gray
+            color = Gray,
+            textAlign = TextAlign.Center
         )
         Text(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = dimensionResource(id = R.dimen.regular_padding)),
             text = move.power?.toString() ?: run {
                 stringResource(id = R.string.no_number_text)
             },
             style = MaterialTheme.typography.bodyMedium,
-            color = Gray
+            color = Gray,
+            textAlign = TextAlign.Center
         )
         Text(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = dimensionResource(id = R.dimen.regular_padding)),
             text = move.accuracy?.toString() ?: run {
                 stringResource(id = R.string.no_number_text)
             },
             style = MaterialTheme.typography.bodyMedium,
-            color = Gray
+            color = Gray,
+            textAlign = TextAlign.Center
         )
     }
 }
--- app/src/main/java/com/testdex/ui/screens/pokedex/pokemon/moves/PokemonMovesListHeader.kt
+++ app/src/main/java/com/testdex/ui/screens/pokedex/pokemon/moves/PokemonMovesListHeader.kt
@@ -3,6 +3,7 @@ package com.testdex.ui.screens.pokedex.pokemon.moves
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.testdex.R
 import com.testdex.ui.composables.TestdexVerticalDivider
 import com.testdex.ui.theme.Gray
@@ -23,16 +25,18 @@ fun PokemonMovesListHeader(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .height(dimensionResource(id = R.dimen.stat_header_height))
             .border(
                 width = dimensionResource(id = R.dimen.divider_thickness),
                 color = Gray,
                 shape = RoundedCornerShape(dimensionResource(id = R.dimen.stat_indicator_corner_radius))
-            )
-            .padding(dimensionResource(id = R.dimen.screen_padding)),
+            ),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            modifier = Modifier.weight(2f),
+            modifier = Modifier
+                .weight(2.6f)
+                .padding(start = dimensionResource(id = R.dimen.screen_padding)),
             text = stringResource(id = R.string.move_name_text),
             style = MaterialTheme.typography.bodyMedium,
             color = MaterialTheme.colorScheme.onBackground
@@ -42,28 +46,32 @@ fun PokemonMovesListHeader(
             modifier = Modifier.weight(1f),
             text = stringResource(id = R.string.type_text),
             style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onBackground
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
         )
         TestdexVerticalDivider()
         Text(
             modifier = Modifier.weight(1f),
             text = stringResource(id = R.string.move_pp_text),
             style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onBackground
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
         )
         TestdexVerticalDivider()
         Text(
             modifier = Modifier.weight(1f),
             text = stringResource(id = R.string.move_power_text),
             style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onBackground
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
         )
         TestdexVerticalDivider()
         Text(
             modifier = Modifier.weight(1f),
             text = stringResource(id = R.string.move_accuracy_text),
             style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onBackground
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
         )
     }
 }
