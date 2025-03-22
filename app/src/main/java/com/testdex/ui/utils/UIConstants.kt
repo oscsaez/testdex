cfb515fb26ec4d1a7e8e8136d70ab7f8c3483aad
oscsaez <oscar.sg.dnh@gmail.com>
2025-03-22 16:46:48 +0100

    ✨ Add variable in PokemonBasicsUIModel to check if its pokedex order is available

--- app/src/main/java/com/testdex/ui/utils/UIConstants.kt
+++ app/src/main/java/com/testdex/ui/utils/UIConstants.kt
@@ -1,9 +1,8 @@
 package com.testdex.ui.utils
 
 object UIConstants {
-    const val MAX_TOTAL_STATS_NUMBER = 255f
 
-    const val MIN_POKEMON_NUMBER_ON_A_PAGE = 0
-    const val MAX_POKEMON_NUMBER_ON_A_PAGE = 10
-    const val NUMBER_OF_ITEMS_UNTIL_RELOAD = 1
+    const val NOT_AVAILABLE_POKEDEX_ORDER = -1
+
+    const val MAX_TOTAL_STATS_NUMBER = 255f
 }
